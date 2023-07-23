56fce8dcc3e1c386eba150d8fe85093f33040019
Mordecai Mengesteab <mordecai.mengesteab@gmail.com>
2023-07-23 00:06:37 -0400

    Midway through college creation form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -53,6 +53,26 @@ export const App = () => {
     setCollege({ ...college, [name]: value });
   };
 
+  const handleRankChange = (event) => {
+    const { name, value } = event.target;
+    setCollege({
+      ...college,
+      programRankings: { ...college.programRankings, [name]: value },
+    });
+  };
+
+  const handleTestChange = (event) => {
+    const { name, value } = event.target;
+    setCollege({ ...college, testRange: { [name]: value } });
+  };
+
+  const publicChange = (event) => {
+    const { value } = event.target;
+    if (value === "public") {
+      setCollege({ ...college, public: true });
+    }
+  };
+
   const onSubmit = async (event) => {
     event.preventDefault();
     try {
@@ -99,6 +119,156 @@ export const App = () => {
           type="text"
           id="shortName"
           name="shortName"
+          onChange={handleChange}
+        />
+
+        <label htmlFor="location">Location</label>
+        <input
+          type="text"
+          id="location"
+          name="location"
+          onChange={handleChange}
+        />
+
+        <label htmlFor="setting">setting</label>
+        <input
+          type="text"
+          id="setting"
+          name="setting"
+          onChange={handleChange}
+        />
+
+        <label htmlFor="campusSize">Campus Size</label>
+        <input
+          type="text"
+          id="campusSize"
+          name="campusSize"
+          onChange={handleChange}
+        />
+
+        <label htmlFor="genRanking">General Ranking</label>
+        <input
+          type="number"
+          id="genRanking"
+          name="genRanking"
+          onChange={handleChange}
+        />
+
+        <label htmlFor="bestValue">Best Value Ranking</label>
+        <input
+          type="number"
+          id="bestValue"
+          name="bestValue"
+          onChange={handleRankChange}
+        />
+
+        <label htmlFor="engineering">Engineering Ranking</label>
+        <input
+          type="number"
+          id="engineering"
+          name="engineering"
+          onChange={handleRankChange}
+        />
+
+        <label htmlFor="biomedEng">Biomed Eng Ranking</label>
+        <input
+          type="number"
+          id="biomedEng"
+          name="biomedEng"
+          onChange={handleRankChange}
+        />
+
+        <label htmlFor="eeEng">Electric Eng Ranking</label>
+        <input
+          type="number"
+          id="eeEng"
+          name="eeEng"
+          onChange={handleRankChange}
+        />
+
+        <label htmlFor="mechEng">Mechanical Eng Ranking</label>
+        <input
+          type="number"
+          id="mechEng"
+          name="mechEng"
+          onChange={handleRankChange}
+        />
+
+        <label htmlFor="compSci">Comp Sci Ranking</label>
+        <input
+          type="number"
+          id="compSci"
+          name="compSci"
+          onChange={handleRankChange}
+        />
+
+        <label htmlFor="ai">Artifical Int Ranking</label>
+        <input type="number" id="ai" name="ai" onChange={handleRankChange} />
+
+        <label htmlFor="cyberSec">Cyber Sec Ranking</label>
+        <input
+          type="number"
+          id="cyberSec"
+          name="cyberSec"
+          onChange={handleRankChange}
+        />
+
+        <label htmlFor="dataScience">Data Science Ranking</label>
+        <input
+          type="number"
+          id="dataScience"
+          name="dataScience"
+          onChange={handleRankChange}
+        />
+
+        <label htmlFor="writing">Writing Ranking</label>
+        <input
+          type="number"
+          id="writing"
+          name="writing"
+          onChange={handleRankChange}
+        />
+
+        <label htmlFor="studyAbroad">Study Abroad Ranking</label>
+        <input
+          type="number"
+          id="studyAbroad"
+          name="studyAbroad"
+          onChange={handleRankChange}
+        />
+
+        <label htmlFor="acceptanceRate">Acceptance Rate</label>
+        <input
+          type="number"
+          id="acceptanceRate"
+          name="acceptanceRate"
+          onChange={handleChange}
+        />
+
+        <label htmlFor="numStudents">Num Students</label>
+        <input
+          type="number"
+          id="numStudents"
+          name="numStudents"
+          onChange={handleChange}
+        />
+
+        <label htmlFor="public">Public</label>
+        <input type="text" id="public" name="public" onChange={publicChange} />
+
+        <label htmlFor="baseCost">Num Students</label>
+        <input
+          type="number"
+          id="baseCost"
+          name="baseCost"
+          onChange={handleChange}
+        />
+
+        <label htmlFor="costAfterAid">Num Students</label>
+        <input
+          type="number"
+          id="costAfterAid"
+          name="costAfterAid"
           onChange={handleChange}
         />
 
