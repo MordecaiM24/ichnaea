5507ec8cb5be4c2d51775b57abb5cae0c6c885c7
Mordecai <mordecai.mengesteab@gmail.com>
2023-08-07 14:28:46 -0600

    Implemented card loading. Slightly taller than other cards.

--- client/src/pages/colleges/College.jsx
+++ client/src/pages/colleges/College.jsx
@@ -2,6 +2,7 @@ import { Plus, PlusLg, Trash3Fill } from "react-bootstrap-icons";
 import "./College.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export const College = (props) => {
   // Is college saved will look at _id and check if it has a match in savedColleges. Each button should be rendered as isSaved ? trash : plus
@@ -34,7 +35,9 @@ export const College = (props) => {
   const length = "4 Year";
 
   const [addBtnVisibility, setVisibility] = useState(false);
-  const [addLoading, setAddLoading] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
   // TODO: Make loading animation for user feedback when adding college. Grey out card, possibly put spinner in the middle.
 
   useEffect(() => {
@@ -49,25 +52,32 @@ export const College = (props) => {
     setVisibility(false);
   };
 
-  const handleClick = async () => {
+  const handleClick = () => {
     if (!userID) {
       alert("You must be logged in to save colleges and scholarships");
+      navigate("/profile");
     } else {
-      if (isCollegeSaved) {
-        const res = await axios.delete(
-          `http://${
-            import.meta.env.VITE_IP
-          }:5000/api/users/removeCollege/${userID}/${_id}`
-        );
-        updateSaved(shouldUpdate + 1);
-      } else {
-        const res = await axios.patch(
-          `http://${import.meta.env.VITE_IP}:5000/api/users/saveCollege`,
-          { userID, collegeToSave: _id }
-        );
-        updateSaved(shouldUpdate + 1);
-      }
+      handleSaveUpdate();
     }
+  };
+
+  const handleSaveUpdate = async () => {
+    setLoading(true);
+    if (isCollegeSaved) {
+      const res = await axios.delete(
+        `http://${
+          import.meta.env.VITE_IP
+        }:5000/api/users/removeCollege/${userID}/${_id}`
+      );
+      updateSaved(shouldUpdate + 1);
+    } else {
+      const res = await axios.patch(
+        `http://${import.meta.env.VITE_IP}:5000/api/users/saveCollege`,
+        { userID, collegeToSave: _id }
+      );
+      updateSaved(shouldUpdate + 1);
+    }
+    setLoading(false);
   };
 
   const imgLinks = {
@@ -88,63 +98,97 @@ export const College = (props) => {
       "https://ucarecdn.com/fe36e611-5930-4fa2-b815-12d7c1a550e2/harvard1.jpg",
   };
 
-  return (
-    <div className="col-12  col-md-6 col-xl-4">
-      <div
-        className="card mb-3 border-primary border-2 p-0 btn btn-outline-primary text-start college-card"
-        onMouseOver={handleMouseOver}
-        onMouseOut={handleMouseOut}
-      >
-        {/* Row gx-0 makes a horizontal card with no space */}
-        <div className="row gx-0">
-          <div className="col-6 col-md-5 bg-primary">
-            <img
-              src={imgLinks[kebabName]}
-              className="img-fluid rounded-start"
-            />
-          </div>
-          <div className="col-6 col-md-7">
-            <div className="card-body p-2" style={{ height: "100%" }}>
-              <div
-                className="card-text d-flex flex-column justify-content-between overflow-hidden"
-                style={{ height: "100%" }}
-              >
-                {/* TODO: change info depending on screen size */}
-                <p className="lead mb-0">
-                  {fullName.length < 30 ? fullName : shortName}
+  if (isLoading) {
+    return (
+      <div className="col-12 col-md-6 col-xl-4">
+        <div class="card h-100 mb-1" aria-hidden="true">
+          <div className="row gx-0 h-100">
+            <div className="col-6 col-md-5  h-100"></div>
+            <div className="col-6 col-md-7  h-100">
+              <div className="card-body p-2 h-100">
+                <p class="lead mb-0 placeholder-glow">
+                  <span class="placeholder col-6"></span>
                 </p>
-                <small className="d-none d-sm-block">
-                  <strong>{location}</strong>
+                <small class="d-none d-sm-block placeholder-glow">
+                  <span className="placeholder col-7"></span>
                 </small>
-                <small className="d-none d-sm-block">
-                  {length} &#183; {privacy ? "Public" : "Private"} &#183;{" "}
-                  {setting}
+                <small class="d-none d-sm-block placeholder-glow">
+                  <span className="placeholder col-12"></span>
                 </small>
-                <small className="d-none d-sm-block">
-                  General Ranking: #{genRanking}
-                  {/* TODO: add (tie) if genRanking matches other colleges genRanking */}
+                <small class="d-none d-sm-block placeholder-glow">
+                  <span className="placeholder col-8"></span>
                 </small>
-                <small className="d-none d-sm-block">
-                  {numStudents.toLocaleString("en-US")} Undergraduate Students
-                  {/* {numStudents} */}
+                <small class="d-none d-sm-block placeholder-glow">
+                  <span className="placeholder col-9"></span>
                 </small>
-                <small className="d-none d-sm-block">{characteristic}</small>
+                <small class="d-none d-sm-block placeholder-glow">
+                  <span className="placeholder col-5"></span>
+                </small>
               </div>
             </div>
           </div>
         </div>
-        {addBtnVisibility && (
-          //TODO: Use isCollegeSaved to see if college is saved, and if so, replace plus button with trash button
-
-          <button
-            className="btn btn-outline-secondary rounded-circle add-college-btn btn-lg border-2"
-            onClick={handleClick}
-          >
-            {isCollegeSaved ? <Trash3Fill /> : <PlusLg />}
-            {/* <PlusLg /> */}
-          </button>
-        )}
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="col-12 col-md-6 col-xl-4">
+        <div
+          className="card mb-3 border-primary border-2 p-0 btn btn-outline-primary text-start college-card"
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        >
+          {/* Row gx-0 makes a horizontal card with no space */}
+          <div className="row gx-0">
+            <div className="col-6 col-md-5 bg-primary">
+              <img
+                src={imgLinks[kebabName]}
+                className="img-fluid rounded-start"
+              />
+            </div>
+            <div className="col-6 col-md-7">
+              <div className="card-body p-2" style={{ height: "100%" }}>
+                <div
+                  className="card-text d-flex flex-column justify-content-between overflow-hidden"
+                  style={{ height: "100%" }}
+                >
+                  {/* TODO: change info depending on screen size */}
+                  <p className="lead mb-0">
+                    {fullName.length < 30 ? fullName : shortName}
+                  </p>
+                  <small className="d-none d-sm-block">
+                    <strong>{location}</strong>
+                  </small>
+                  <small className="d-none d-sm-block">
+                    {length} &#183; {privacy ? "Public" : "Private"} &#183;{" "}
+                    {setting}
+                  </small>
+                  <small className="d-none d-sm-block">
+                    General Ranking: #{genRanking}
+                    {/* TODO: add (tie) if genRanking matches other colleges genRanking */}
+                  </small>
+                  <small className="d-none d-sm-block">
+                    {numStudents.toLocaleString("en-US")} Undergraduate Students
+                    {/* {numStudents} */}
+                  </small>
+                  <small className="d-none d-sm-block">{characteristic}</small>
+                </div>
+              </div>
+            </div>
+          </div>
+          {addBtnVisibility && (
+            //TODO: Use isCollegeSaved to see if college is saved, and if so, replace plus button with trash button
+
+            <button
+              className="btn btn-outline-secondary rounded-circle add-college-btn btn-lg border-2"
+              onClick={handleClick}
+            >
+              {isCollegeSaved ? <Trash3Fill /> : <PlusLg />}
+              {/* <PlusLg /> */}
+            </button>
+          )}
+        </div>
+      </div>
+    );
+  }
 };
