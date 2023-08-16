822bda103e0592293cba1999832340f7519c27c4
Mordecai <mordecai.mengesteab@gmail.com>
2023-08-16 13:41:38 -0700

    CSS changes for search bar on college grid

--- client/src/pages/colleges/CollegeGrid.jsx
+++ client/src/pages/colleges/CollegeGrid.jsx
@@ -2,6 +2,7 @@ import { College } from "./College";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { Search } from "react-bootstrap-icons";
+import { useSearchParams } from "react-router-dom";
 
 export const CollegeGrid = () => {
   const [colleges, setColleges] = useState([]);
@@ -11,6 +12,11 @@ export const CollegeGrid = () => {
   const [shouldUpdate, updateSaved] = useState(0);
 
   const userID = window.localStorage.getItem("userID");
+
+  const [params] = useSearchParams();
+
+  // Get a specific query parameter
+  const searchQuery = params.get("search");
 
   useEffect(() => {
     const getColleges = async () => {
@@ -42,17 +48,18 @@ export const CollegeGrid = () => {
 
   return (
     <div className="container-fluid px-5">
-      <div className="search-bar-container  position-relative">
-        <div className="search-box d-flex align-items-center py-4">
+      <div className="search-bar-container position-relative">
+        <form className="search-box d-flex align-items-center py-4">
           <input
             type="text"
             className="search-input"
             placeholder="Start Looking For Something!"
+            name="search"
           />
-          <a className="search-btn" href="#">
+          <a className="search-btn" type="submit">
             <Search />
           </a>
-        </div>
+        </form>
       </div>
 
       <div className="row gx-5 gy-5">
