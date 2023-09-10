162823fa2a18959a6ced509a4bba5ce37f2f3cf6
Mordecai <mordecai.mengesteab@gmail.com>
2023-09-10 19:26:35 -0400

    Light renaming

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import { userRouter } from "./routes/users.js";
 import { collegeRouter } from "./routes/colleges.js";
 import { Server } from "socket.io";
 import { DocumentModel } from "./models/Document.js";
+import { createServer } from "http";
 
 const app = express();
 
@@ -16,8 +17,9 @@ app.use("/api/colleges", collegeRouter);
 app.use("/api/users", userRouter);
 
 app.get("/", (req, res, next) => {
-  res.status(200).send("Hello, world!");
+  res.status(200).send("Listening on port 3002!");
 });
+
 mongoose.connect(
   // `mongodb+srv://${process.env.ATLAS_ID}:${process.env.ATLAS_PASSWORD}@cluster0.rnqhvef.mongodb.net/production?retryWrites=true&w=majority`
   `mongodb+srv://${process.env.ATLAS_ID}:${process.env.ATLAS_PASSWORD}@cluster0.rnqhvef.mongodb.net/?retryWrites=true&w=majority`
@@ -25,19 +27,23 @@ mongoose.connect(
   // `mongodb://localhost:27017`
 );
 
-app.listen(5000, async () => {
+const expressServer = app.listen(5000, async () => {
   console.log("Server listening on port 5000...");
 });
 
 //socket connection
 
-const io = new Server(3001, {
-  cors: {
-    // origin: "http://localhost:5173",
-    // origin: "http://192.168.1.10:5173",
-    methods: ["GET", "POST"],
-  },
-});
+// const io = new Server(httpServer, { cors: { origin: "*" } });
+
+// const io = new Server(3001, {
+//   cors: {
+//     // origin: "http://localhost:5173",
+//     // origin: "http://192.168.1.10:5173",
+//     // origin: "https://universitrack.com",
+//     methods: ["GET", "POST"],
+//   },
+// });
+const io = new Server(expressServer, { cors: { origin: "*" } });
 
 const defaultValue = {
   ops: [
@@ -82,3 +88,5 @@ async function findOrCreateDocument(id) {
   if (document) return document;
   return await DocumentModel.create({ _id: id, data: defaultValue });
 }
+
+// httpServer.listen(3001);
