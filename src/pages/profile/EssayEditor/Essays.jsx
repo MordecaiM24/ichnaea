54b622690519dd12c1a82ab06d924bf6ae5f47ec
Mordecai <mordecai.mengesteab@gmail.com>
2024-04-17 22:23:10 -0400

    Added autosave and save on AI change.

--- src/pages/profile/EssayEditor/Essays.jsx
+++ src/pages/profile/EssayEditor/Essays.jsx
@@ -48,13 +48,25 @@ export function Essays() {
 function Essay({ essay }) {
   const [response, setResponse] = useState(essay.response);
   const [isAILoading, setAILoading] = useState(false);
+  const [changeDelta, setChangeDelta] = useState(0);
+  const [isEssaySaving, setEssaySaving] = useState(false);
 
-  async function saveEssay() {
+  async function saveEssay(userResponse = response) {
+    setEssaySaving(true);
     await supabase
       .from("user_supplemental_essays")
-      .update({ response })
+      .update({ response: userResponse })
       .eq("id", essay.id);
+
+    setEssaySaving(false);
   }
+
+  useEffect(() => {
+    if (changeDelta === 20) {
+      saveEssay();
+      setChangeDelta(0);
+    }
+  }, [changeDelta]);
 
   async function edit() {
     if (response.length < 100) {
@@ -73,21 +85,21 @@ function Essay({ essay }) {
 
       return;
     }
-    await new Promise((resolve) => setTimeout(null, 100000));
 
-    // const editedEssay = await axios.post(
-    //   `${import.meta.env.VITE_FUNCTION_ENDPOINT}/api/edit`,
-    //   { essay: response },
-    // );
+    const editedEssay = await axios.post(
+      `${import.meta.env.VITE_FUNCTION_ENDPOINT}/api/edit`,
+      { essay: response },
+    );
 
-    // const originalEssay = response;
+    const originalEssay = response;
 
-    // const newEssay = originalEssay.concat(
-    //   "\n\nEdited Essay: \n\n",
-    //   editedEssay.data.final_essay,
-    // );
+    const newEssay = originalEssay.concat(
+      "\n\nEdited Essay: \n\n",
+      editedEssay.data.final_essay,
+    );
 
-    // setResponse(newEssay);
+    setResponse(newEssay);
+    saveEssay(newEssay);
   }
 
   function critique() {
@@ -123,6 +135,7 @@ function Essay({ essay }) {
           <div className="relative min-h-56 w-full">
             <textarea
               onChange={(e) => {
+                setChangeDelta((changeDelta) => changeDelta + 1);
                 e.target.style.height = "inherit";
                 e.target.style.height = `${e.target.scrollHeight + 18}px`;
                 setResponse(e.target.value);
@@ -134,6 +147,7 @@ function Essay({ essay }) {
               className="flex h-fit min-h-56 w-full rounded-md border border-input bg-background px-3 py-2 text-sm placeholder:text-muted-foreground"
               value={response}
             />
+
             {isAILoading && (
               <div className="primary absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center bg-gray-100 bg-opacity-50">
                 <Circles
@@ -142,6 +156,32 @@ function Essay({ essay }) {
                   color="rgb(145,0,22)"
                   wrapperClass="flex items-center justify-center"
                 />
+              </div>
+            )}
+
+            {isEssaySaving && (
+              <div className="primary absolute bottom-3 right-3 flex h-6 w-fit items-center justify-center gap-x-2 animate-in">
+                <p className="animate-pulse text-sm text-gray-500">Saving</p>
+                <svg
+                  className="h-5 w-5 animate-spin text-gray-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="-3 -3 30 30"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  />
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  />
+                </svg>
               </div>
             )}
           </div>
